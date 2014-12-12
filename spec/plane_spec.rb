03c6dae881e170c963c2eae2bb18d760c7951cea
Kieran Goodacre <kierangoodacre@Kierans-MacBook-Pro-2.local>
2014-12-12 17:23:48 +0000

    refactored code

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,20 +1,19 @@
 require './lib/plane'
 
 describe Plane do
+
+  let (:plane) {Plane.new}			
     
   it 'should not take off after we create it' do
-    plane = Plane.new
     expect(plane).not_to be_in_the_air
   end
 
   it 'should be able to take off' do
-  	plane = Plane.new
   	plane.take_off!
   	expect(plane).to be_in_the_air
   end
 
   it "should be able to land" do
-  	plane = Plane.new
   	plane.take_off!
   	plane.land!
   	expect(plane).not_to be_in_the_air
